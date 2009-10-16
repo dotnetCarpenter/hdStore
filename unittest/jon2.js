aded71da457e6c56cc8cf48d847a236e4a9e0586
dotnetcarpenter <jon.ronnenberg@gmail.com>
2009-10-16 12:10:51 +0200

    added unit tests for event handling

---
+++ unittest/jon2.js
@@ -0,0 +1,33 @@
+/**
+ * @author Jon Ege Ronnenberg
+ */
+$(document).ready(function(){
+	module("Unit test of hdStore event handling");
+	test('hdStore.save method', function(){
+		expect(2);
+		
+		var store = new hdStore();
+		store.addHandler({
+			type: 'save',
+			handler: function(){
+				document.saveHandlerValue = "Hello world from save";
+			}
+		});
+		ok(store.save(), 'Handler is found');
+		equals(document.saveHandlerValue, "Hello world from save");
+	});
+	test('hdStore.load method', function(){
+		expect(2);
+		
+		var store = new hdStore();
+		store.addHandler({
+			type: 'load',
+			handler: function(){
+				document.loadHandlerValue = "Hello world from load";
+			}
+		});
+		ok(store.load(), 'Handler is found');
+		equals(document.loadHandlerValue, "Hello world from load");
+	});
+//	module("Unit test of IE implementation of hdStore");
+});
