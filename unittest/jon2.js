e10bc4861ba3a83375fc442ea6c9f96985c46bcd
dotnetcarpenter <jon.ronnenberg@gmail.com>
2009-10-17 17:27:28 +0200

    added unit test for scope in handler objects

--- unittest/jon2.js
+++ unittest/jon2.js
@@ -4,17 +4,20 @@
 $(document).ready(function(){
 	module("Unit test of hdStore event handling");
 	test('hdStore.save method', function(){
-		expect(2);
+		expect(3);
 		
 		var store = new hdStore();
 		store.addHandler({
 			type: 'save',
 			handler: function(){
 				document.saveHandlerValue = "Hello world from save";
-			}
+				this.saveHandlerValue = "Hello world from save";
+			},
+			scope: hdStore.prototype
 		});
 		ok(store.save(), 'Handler is found');
 		equals(document.saveHandlerValue, "Hello world from save");
+		equals(hdStore.prototype.saveHandlerValue, "Hello world from save");
 	});
 	test('hdStore.load method', function(){
 		expect(2);
