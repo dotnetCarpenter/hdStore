380e4a8aefa1eb1fe001fc1edab4758eb99a9131
dotnetcarpenter <jon.ronnenberg@gmail.com>
2009-11-08 19:00:18 +0100

    removed scope test

--- unittest/jon2.js
+++ unittest/jon2.js
@@ -4,20 +4,17 @@
 $(document).ready(function(){
 	module("Unit test of hdStore event handling");
 	test('hdStore.save method', function(){
-		expect(3);
+		expect(2);
 		
 		var store = new hdStore();
 		store.addHandler({
 			type: 'save',
 			handler: function(){
-				document.saveHandlerValue = "Hello world from save";
 				this.saveHandlerValue = "Hello world from save";
-			},
-			scope: hdStore.prototype
+			}
 		});
 		ok(store.save(), 'Handler is found');
-		equals(document.saveHandlerValue, "Hello world from save");
-		equals(hdStore.prototype.saveHandlerValue, "Hello world from save");
+		equals(hdStore.saveHandlerValue, "Hello world from save");
 	});
 	test('hdStore.load method', function(){
 		expect(2);
