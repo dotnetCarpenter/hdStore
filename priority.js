a03462b0455155b5b0b658137f026fe939ca5a5a
dotnetcarpenter <jon.ronnenberg@gmail.com>
2009-11-26 02:10:34 +0100

    added localStore

--- priority.js
+++ priority.js
@@ -1,9 +1,12 @@
 /**
  * Priorities which plug-in to use to save and load data.
  * @author Jon Ege Ronnenberg
- * @version 0.1
+ * @version 0.2
  */
 hdStore.Priorities = {
+	localStorage: {
+		canBeUsed: hdStore.localStorage.canBeUsed
+	},
 	userData: {
 		canBeUsed: hdStore.userData.canBeUsed
 	}
