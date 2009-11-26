63e87b3844980f0723220836b3e96255c290aee1
dotnetcarpenter <jon.ronnenberg@gmail.com>
2009-11-26 02:11:08 +0100

    first implementation of localStore

--- localStorage/localStorage.js
+++ localStorage/localStorage.js
@@ -5,19 +5,37 @@
  * http://github.com/dotnetCarpenter/hdStore/issues#issue/10
  */
 hdStore.localStorage = function(){
+	function _handler(store, type){
+		if(!hdStore.localStorage.canBeUsed){ return; }
+		try {
+			if (type == 'save') {
+				localStorage[store.id] = JSON.stringify(store.toArray3());
+			}
+			else {
+				var dataArray = JSON.parse(localStorage[store.id]);
+				for (var key in dataArray) {
+					store.add(key, dataArray[key]);
+				}
+			}
+			return true;
+		} catch(e){
+			if(console){ console.log(e); }
+			return false;
+		}
+	};
 	hdStore.prototype.addHandler({
 		type: 'save',
 		handler: function(){
-			
+			return _handler(this, 'save');
 		}
 	});
 	hdStore.prototype.addHandler({
 		type: 'load',
 		handler: function(){
-			
+			return _handler(this, 'load');
 		}
 	});
 	return {
 		canBeUsed: localStorage ? true : false
 	}
-};
+}();
