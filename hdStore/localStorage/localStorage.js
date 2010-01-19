1eca897c98abecf51c5533bd84d92251f7c71c37
dotnetcarpenter <jon.ronnenberg@gmail.com>
2010-01-19 17:57:00 +0100

    fixed serialization bug

--- hdStore/localStorage/localStorage.js
+++ hdStore/localStorage/localStorage.js
@@ -1,38 +1,46 @@
 /**
  * @author Jon Ege Ronnenberg
- * @version 0.1
+ * @version 0.2
  * @classDescription hdStore plug-in using localStore to persist data
  * http://github.com/dotnetCarpenter/hdStore/issues#issue/10
  */
 hdStore.localStorage = function(){
-	function _handler(store, type){
+	function _handler(store, type, dict){
 		if(!hdStore.localStorage){ return; }
 		try {
 			if (type == 'save') {
-				localStorage[store.id] = JSON.stringify(store.toArray());
+				localStorage[store.id] = JSON.stringify(dict);
+				console.log(dict);
 			}
 			else {
-				var dataArray = JSON.parse(localStorage[store.id]);
+				dict = JSON.parse(localStorage[store.id]);
+				for (var key in dict){
+					store.add(key, dict[key]);
+				}
+			/*	old code (delete if this passes unit tests)
+			 var dataArray = JSON.parse(localStorage[store.id]);
+				if(dataArray[0] == null){ console.log('localStorage',null); }// if the array is null exit or null will be added to hdStore
 				for (var key in dataArray) {
 					store.add(key, dataArray[key]);
 				}
+			*/
 			}
 			return true;
 		} catch(e){
-			if(console){ console.log(e); }
+			if(console && console.log){ console.log(e); }
 			return false;
 		}
 	};
 	hdStore.prototype.addHandler({
 		type: 'save',
-		handler: function(){
-			return _handler(this, 'save');
+		handler: function(dict){
+			return _handler(this, 'save', dict);
 		},
 		id: 'localStorage'
 	});
 	hdStore.prototype.addHandler({
 		type: 'load',
-		handler: function(){
+		handler: function(dict){
 			return _handler(this, 'load');
 		},
 		id: 'localStorage'
