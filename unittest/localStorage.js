c5a0733f3b63d58519903e39a3ef9118fa17cc75
dotnetCarpenter <jon.ronnenberg@gmail.com>
2010-05-27 18:10:17 +0200

    boiler plate code

---
+++ unittest/localStorage.js
@@ -0,0 +1,20 @@
+/**
+ * Unit tests of localStorage
+ * @author Jon Ege Ronnenberg
+ * @version 0.1
+ */
+module("hdStore localStorage Test", {
+	setup: function(){
+		
+	},
+	teardown: function(){
+		
+	}
+});
+
+test('save', function(){
+	
+});
+test('load', function(){
+	
+});
