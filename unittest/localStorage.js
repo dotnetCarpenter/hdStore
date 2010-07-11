bc0f668079f33876e37d2aa357e00f040c4f5b87
dotnetCarpenter <jon.ronnenberg@gmail.com>
2010-07-11 17:55:34 +0200

    added unit test for localStorage

--- unittest/localStorage.js
+++ unittest/localStorage.js
@@ -5,16 +5,20 @@
  */
 module("hdStore localStorage Test", {
 	setup: function(){
-		
+		this.myObject = { hep: "123", food: ["tofu", "tomatos"] };
+		this.store = new hdStore('localStorage');
 	},
 	teardown: function(){
-		
+		this.store.removeAll();
 	}
 });
 
-test('save', function(){
-	
+test('save', 1, function(){
+	this.store.add("myObject", this.myObject);
+	ok(this.store.save(), "The data in " + this.store.id + " is saved");
+	this.store.removeAll();
 });
-test('load', function(){
-	
+test('load', 2, function(){
+	ok(this.store.load(), "The data in " + this.store.id + " is loaded");
+	same(this.myObject, this.store.getItem("myObject"));
 });
