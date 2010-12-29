aa32fa41c29e9b7ed8ba95d28f3bb474738ea31b
dotnetCarpenter <jon.ronnenberg@gmail.com>
2010-12-29 15:52:23 +0100

    clean up and prepare alfa download

--- src/priority.js
+++ src/priority.js
@@ -1,13 +1,1 @@
-/**
- * Priorities which plug-in to use to save and load data.
- * @author Jon Ege Ronnenberg
- * @version 0.2
- */
-hdStore.Priorities = {
-	localStorage: {
-		canBeUsed: hdStore.localStorage
-	},
-	userData: {
-		canBeUsed: hdStore.userData
-	}	
-};
+hdStore.Priorities={localStorage:{canBeUsed:hdStore.localStorage},userData:{canBeUsed:hdStore.userData}};
